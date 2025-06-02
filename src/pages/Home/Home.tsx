7e27881485d6345c2d88540bfb1aae31435113d4
Andrew <aunderwood797@gmail.com>
2025-06-02 17:01:57 -0500

    Adding more components.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from '../../components/Header.tsx';
 
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the home page!</p>
+      <Header />
+      <div>
+        <h1>Home Page</h1>
+        <p>Welcome to the home page!</p>
+      </div>
     </div>
   );
 };
