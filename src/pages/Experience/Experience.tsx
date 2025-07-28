b255903d69ae47b67c06f91f78ddf4ed5375a717
Andrew <aunderwood797@gmail.com>
2025-07-28 06:26:16 -0500

    Fixing spacing.

--- src/pages/Experience/Experience.tsx
+++ src/pages/Experience/Experience.tsx
@@ -101,7 +101,7 @@ const Experience: React.FC = () => {
                                 <p>
                                     Started my professional journey in a collaborative environment.
                                     Focused on <Property>automated testing</Property> while learning
-                                    <Keyword>industry best practices</Keyword> and <Keyword>collaborative workflows</Keyword>.
+                                    <Keyword> industry best practices</Keyword> and <Keyword>collaborative workflows</Keyword>.
                                 </p>
 
                                 <ul className="ml-6 space-y-2 list-disc">
